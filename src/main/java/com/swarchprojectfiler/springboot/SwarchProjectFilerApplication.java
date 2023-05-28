d18429b49ba64f5392fb4e60e3164f6891425560
selimcetin <selim-cetin@web.de>
2023-05-29 00:02:13 +0200

    Initial commit

---
+++ src/main/java/com/swarchprojectfiler/springboot/SwarchProjectFilerApplication.java
@@ -0,0 +1,13 @@
+package com.swarchprojectfiler.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwarchProjectFilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwarchProjectFilerApplication.class, args);
+	}
+
+}
